f1df2750738fb1a6368d48328e14176600a87271
fastoch <fastoch@ik.me>
2025-08-24 11:36:04 +0200

    @35

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,10 @@ import './App.css'
 import MovieCard from './components/MovieCard'  
 
 function App() {
-  const movieNumber = 2
 
   return (
     <>
-      {movieNumber === 1 ? (
-        <MovieCard movie={{title: "The Matrix", release_date: "1999", url: ""}} /> 
-      ) : (
-        <MovieCard movie={{title: "Back to the Future", release_date: "1985", url: ""}} />
-      )}
+      
     </>
   )
 }
