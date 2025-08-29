83211a064950a4f4b88d207cb652a274fac86ab6
fastoch <fastoch@ik.me>
2025-08-29 17:50:33 +0200

    @89/99

--- src/contexts/MovieContext.jsx
+++ src/contexts/MovieContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useContext, useEffect } from 'react'
+import { createContext, useState, useContext, useEffect, use } from 'react'
 
 // first, we create the context
 const MovieContext = createContext()
@@ -11,13 +11,18 @@ export const MovieProvider = ({children}) => {
   // here goes the logic related to the favorite movies
   const [favorites, setFavorites] = useState([])
 
+  // when the page first loads, check localStorage and update the favorites state  
   useEffect(() => {
     const storedFavorites = localStorage.getItem('favorites')
-    // do we have any favorite movies yet?
     if (storedFavorites) {
       setFavorites(JSON.parse(storedFavorites))
     }
   }, [])
+
+  // when the favorites state changes (movie added or removed), update localStorage
+  useEffect(() => {
+    localStorage.setItem('favorites', JSON.stringify(favorites))
+  }, [favorites])
 
   return <MovieContext.Provider>{children}</MovieContext.Provider>
 }
