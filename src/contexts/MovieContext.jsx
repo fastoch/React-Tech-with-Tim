77698cad8c21eb0c97e05977762f3bc941a5dc9f
fastoch <fastoch@ik.me>
2025-08-29 17:36:01 +0200

    @87/99

--- src/contexts/MovieContext.jsx
+++ src/contexts/MovieContext.jsx
@@ -6,11 +6,27 @@ const MovieContext = createContext()
 // then, we create the hook to grab the value that will be passed to the provider
 export const useMovieContext = () => useContext(MovieContext)
 
-// finally, we create the provider
-export const MovieProvider = () => {
+// finally, we create the provider 
+export const MovieProvider = ({children}) => {
+  // here goes the logic related to the favorite movies
+  const [favorites, setFavorites] = useState([])
 
+  useEffect(() => {
+    const storedFavorites = localStorage.getItem('favorites')
+    // do we have any favorite movies yet?
+    if (storedFavorites) {
+      setFavorites(JSON.parse(storedFavorites))
+    }
+  }, [])
+
+  return <MovieContext.Provider>{children}</MovieContext.Provider>
 }
 
 /**
- * The provider will provide state to any of the components that are wrapped inside of it
+ * The provider will provide data (value) to any of the components that are nested inside of it
+ */
+
+/**
+ * children is a reserved prop.  
+ * in our case, it represents any components or JSX elements that are nested inside <MovieProvider>
  */
